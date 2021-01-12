68b7ac27fe909383c3a66bf86ca85457b0595d97
Rodrigo Cabrera Castaldoni <rodrigo.ccastaldoni@hsl.org.br>
2021-01-12 10:47:16 -0300

    ...

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+import seaborn as sns
+from functools import partial
+from collections import namedtuple
 
 def select_best(df:pd.DataFrame):
     best_fit = df.sort_values(by = "error").reset_index().iloc[-1]
@@ -30,10 +33,12 @@ def exponential_cicle(x, a, b, c):
     return -a*np.exp(-b*x)+c
 
 
-def find_best_b(df:pd.DataFrame, raw_pressures:np.ndarray, b_percentage_range:tuple=(0.05,0.125), step:float=0.001, plot_figure:bool=True):
+def find_best_b(ser, b_percentage_range:tuple=(0.05,0.125), step:float=0.001):
 
-    params, best_fit_func = select_best(df)
-
+    params = ser["param"]
+    fit_func = ser["function"]
+    raw_data = ser["raw_data"]
+    
     a = params[0]
     b = params[1]
 
@@ -46,9 +51,9 @@ def find_best_b(df:pd.DataFrame, raw_pressures:np.ndarray, b_percentage_range:tu
     for b_percentage in np.arange(b_initial, b_end, 0.001):
 
         offSet = (b_percentage*b+a)
-        initial_pressure = raw_pressures[0,0]
+        initial_pressure = raw_data[0,0]
 
-        guess_zero = find_zero(offSet, best_fit_func, params, initial_pressure, learning_rate = 0.01)
+        guess_zero = find_zero(offSet, fit_func, params, initial_pressure, learning_rate = 0.01)
         guess_zero_lst.append(guess_zero)
         
         try:
@@ -57,9 +62,9 @@ def find_best_b(df:pd.DataFrame, raw_pressures:np.ndarray, b_percentage_range:tu
             """
             b_exp_run_lst = []
 
-            for i in range(4, len(raw_pressures)+1, 2):
+            for i in range(4, len(raw_data)+1, 2):
                 #Select first two points
-                run = raw_pressures[i-2:i,:]
+                run = raw_data[i-2:i,:]
                 #Add guess_zero
                 run = np.vstack([guess_zero,run])
                 #Fit curve with 3 points
@@ -72,28 +77,30 @@ def find_best_b(df:pd.DataFrame, raw_pressures:np.ndarray, b_percentage_range:tu
 
         except Exception as e:
             print(e)
-            
-    if plot_figure:
-        data=pd.DataFrame([b_percentage_lst,b_var_lst]).T.rename(columns = {0:"b_percentage", 1:"b_var"}).astype(float)
-        
-        fig = plt.figure(figsize = (8,6))
-        ax = sns.scatterplot(x="b_percentage", y="b_var", data=data)
-        plt.tight_layout()
-        plt.show()
-        
+                    
     index_best_b = np.argmin(b_var_lst)
     return b_percentage_lst[index_best_b], guess_zero_lst[index_best_b]
 
 
-def plot_exponentials(best_b, guess_zero, df, raw_data, func):
+def plot_exponentials(ser, aux_func):
+
+    
+    best_b = ser["best_b"]
+    
+    func = partial(aux_func, best_b)
+    
+    guess_zero = ser["guess_zero"]
+    raw_data = ser["raw_data"]
+    
+    new_pressures = np.arange(int(guess_zero[0]), 100, 1)
+    colors = ["b","g","r","m","y"]
+    
+    exponential_lst = []
+    exponential_tuple = namedtuple("exponential",['x', 'y'])
     
     ######################################################################################################
     plt.figure(figsize = (14,8))
     plt.scatter(guess_zero[0],guess_zero[1], c = 'k', s = 100, label = "ZERO")
-
-    new_pressures = range(int(guess_zero[0]), 100, 7)
-
-    colors = ["b","g","r","m","y"]
 
     for i, cor in zip(range(2, raw_data.shape[0]+1, 2), colors):
 
@@ -101,15 +108,15 @@ def plot_exponentials(best_b, guess_zero, df, raw_data, func):
         run = np.vstack([guess_zero,run])
 
         parameters, pcov = curve_fit(func, run[:,0], run[:,1], method="lm")
-
         new_volumes = func(new_pressures, *parameters)
+        exponential_lst.append(exponential_tuple(new_pressures, new_volumes))
+        
         plt.plot(new_pressures, new_volumes, c=cor, label = f"exponential {i}")
         plt.scatter(raw_data[i-2:i,0], raw_data[i-2:i,1], c=cor, label = f"Pair {i}")
         ##
 
     ##Plotando best fit
-    best_fit = df.sort_values(by = "error").reset_index().iloc[0]
-    new_volumes = best_fit["function"](new_pressures,*best_fit["param"])
+    new_volumes = ser["function"](new_pressures,*ser["param"])
     plt.scatter(new_pressures,new_volumes, c = 'black', marker = '+', label = "Fit")
 
     plt.xlabel("pressure")
@@ -118,4 +125,5 @@ def plot_exponentials(best_b, guess_zero, df, raw_data, func):
     print(f"Best zero: {guess_zero}")
     plt.legend()
     plt.show()
+    return exponential_lst
     
